f09856c94b322f919e3fd328c11d5f4c0a55e1c8
mavo97 <mavodeveloper@gmail.com>
2019-11-11 21:29:16 -0600

    sql

--- eventosdb.sql
+++ eventosdb.sql
@@ -81,5 +81,3 @@ ON DELETE CASCADE;
 
 ALTER TABLE eventos MODIFY estado varchar(1) not null;
 
-insert into eventos(nombre, fecha_inicio, fecha_fin, ubicacion, costo, estado, descripcion)
-	values('sss', '2222-02-02', '2222-02-02', 'sss', '200', FALSE, 'sss');
