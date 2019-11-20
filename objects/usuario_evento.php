d02356dcc9b228460721d32e00c3e9c992a3a95a
mavo97 <mavodeveloper@gmail.com>
2019-11-20 13:59:20 -0600

    eventos del usuario

---
+++ objects/usuario_evento.php
@@ -0,0 +1,112 @@
+<?php
+class UsuariosEvento{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "usuarios_evento";
+ 
+    // object properties
+    public $id_evento;
+    public $id_usuario;
+    public $nombre;
+    public $correo;
+    public $fecha_inicio;
+    public $fecha_fin;
+    public $ubicacion;
+    public $costo;
+    public $estado;
+    public $descripcion;
+
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // registrar usuarios_evento
+    function registrar(){
+    
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "(id_evento, id_usuario) VALUES (?,?);";
+    
+        // prepare query
+        $sql = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->id_evento=htmlspecialchars(strip_tags($this->id_evento));
+        $this->id_usuario=htmlspecialchars(strip_tags($this->id_usuario));
+    
+        // bind values
+        $sql->bindParam(1, $this->id_evento,PDO::PARAM_STR);
+        $sql->bindParam(2, $this->id_usuario, PDO::PARAM_STR);
+       
+    
+        // execute query
+        if($sql->execute()){
+            return true;
+        }
+    
+        return false;
+        
+    }
+    function usuarioRegistrado(){
+
+    $query = "SELECT id_usuario, id_evento FROM " . $this->table_name . "
+            WHERE id_usuario = ? AND id_evento = ?
+            LIMIT 0,1";
+
+    $sql = $this->conn->prepare( $query );
+
+    $this->id_usuario=htmlspecialchars(strip_tags($this->id_usuario));
+    $this->id_evento=htmlspecialchars(strip_tags($this->id_evento));
+
+    $sql->bindParam(1, $this->id_usuario);
+    $sql->bindParam(2, $this->id_evento);
+
+    $sql->execute();
+
+    $num = $sql->rowCount();
+
+    if($num>0){
+
+        $row = $sql->fetch(PDO::FETCH_ASSOC);
+
+        $this->id_usuario = $row['id_usuario'];
+        $this->id_evento = $row['id_evento'];
+
+        return true;
+    }
+
+    return false;
+    }
+    function readEventosu(){
+     
+        // query to read single record
+
+        $query = "SELECT u.correo, e.nombre, ue.id_evento, ue.id_usuario
+            FROM
+                 usuarios u
+            INNER JOIN
+                 usuarios_evento ue on u.id_usuario = ue.id_usuario
+            LEFT JOIN 
+                 eventos e on e.id_evento = ue.id_evento
+            WHERE 
+                 u.id_usuario = ?";
+     
+        // prepare query statement
+        $sql = $this->conn->prepare( $query );
+     
+        // bind id of product to be updated
+        $sql->bindParam(1, $this->id_usuario);
+     
+        // execute query
+        $sql->execute();
+     
+        // get retrieved row
+        return $sql;
+     
+
+
+    }
+}
+?>
---
+++ usuarios_evento/eventosusuario.php
@@ -0,0 +1,68 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/usuario_evento.php';
+ 
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize object
+$useven = new UsuariosEvento($db);
+ 
+
+$useven->id_usuario =isset($_GET['id']) ? $_GET['id'] : die();
+ 
+// query products
+$sql = $useven->readEventosu();
+$num = $sql->rowCount();
+ 
+// check if more than 0 record found
+
+if($num>0){
+ 
+    // products array
+    $eventsUs=array();
+    $eventsUs["records"]=array();
+ 
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $sql->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+ 
+       $eventsUs_item=array(
+            "id_evento" => $id_evento,
+            "id_usuario" => $id_usuario,
+            "nombre" => $nombre,
+            "correo" => $correo,
+
+        );
+ 
+        array_push($eventsUs["records"], $eventsUs_item);
+    }
+ 
+    // set response code - 200 OK
+    http_response_code(200);
+ 
+    // show products data
+    echo json_encode($eventsUs);
+}
+ 
+else{
+    // set response code - 404 Not found
+    http_response_code(404);
+ 
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "EL usuario no tiene eventos.")
+    );
+}
+?>
